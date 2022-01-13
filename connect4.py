8b416dcfd6a00edb8850732363fc0252adf4eddb
Osas <Osaghae2305@gmail.com>
2022-01-13 12:12:56 +0000

    There is a problem where if there is 4 in a row in the bottom it still returns 0

--- connect4.py
+++ connect4.py
@@ -5,6 +5,10 @@ def showBoard (board):
         num += 1
         print(str(num)+"|"+" ".join(row)+"|")
 
+def showScore (points):
+    print("Player 1:",points[0])
+    print("Player 2:",points[1])
+
 def dropD(board,user,col):
     row = 5
     check = True
@@ -23,21 +27,31 @@ def dropD(board,user,col):
         
     return board
 
-def checkland(board,user):
+def checkland(board):
     countY = 0
     countR = 0
     
-
-
     for row in board:
-        countY = 0
-        countR = 0
-        for col in row:
-            if col == "Y":
-                countY += 1
-            elif col == "R":
-                countR += 1
+        if countY == 4 or countR == 4:
+            return 1
+            break
+        else:
+            countY = 0
+            countR = 0
+            for col in row:
+                if col == "Y":
+                    countY += 1
+                    countR = 0
+    
+                if col == "R":
+                    countR += 1
+                    countY = 0
+                
+    return 0
+    
 
+
+            
             
             
 
@@ -45,13 +59,15 @@ def checkland(board,user):
     
 
 
-myBoard = [[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."]]
+myBoard = [[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],[".",".",".",".",".","."],["Y","Y","Y",".",".","."]]
 run = True
 player = 1
+scores = [0,0]
 
 print("Player 1 is Y\n Player 2 is R")
 
 while run:
+    showScore(scores)
     
     print("##########  Player",player,"Turn  ############")
     showBoard(myBoard)
@@ -61,12 +77,16 @@ while run:
 
     showBoard(myBoard)
 
+    scores[player-1] += checkland(myBoard)
+
+
     if player == 1:
         player += 1
     else:
         player -= 1
 
-    run = checkland(myBoard,player)
+
+    
 
 
 ####### Test ########

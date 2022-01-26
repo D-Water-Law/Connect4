9ccff226b9a118e481775714d1a552c1bf458c01
Osas S <Osaghae2305@gmail.com>
2022-01-26 14:51:21 +0000

    Created a connect4 Board

--- connect4Pygame.py
+++ connect4Pygame.py
@@ -1,24 +1,31 @@
 import pygame
 from connect4 import Board
 
-def genBoard(surface,sWidth,sHeight):
-    pass
-    board = Board.getBoard()
-    for row in board:
-        for col in range(len(row)):
-            pygame.draw.rect(surface,(0,0,255),(sWidth/board,sWidth))#############
+def genBoard(surface,board):
+    rowCount = len(board)
+    colCount = len(board[0])
+    
+    
+    
+    
+    squareSize = 100
+    for row in range(rowCount):
+        for col in range(colCount):
+            pygame.draw.rect(surface,(0,0,255),(col*squareSize,row*squareSize+squareSize,squareSize,squareSize))#############
+            pygame.draw.circle(surface,(0,0,0),(col*squareSize+(squareSize/2),row*squareSize+squareSize+(squareSize/2)),40)
         
 
 
 pygame.init()
 win_height = 700
-win_width = 800 
+win_width = 600 
 DISPLAYSURF = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption('Connect 4')
-
 run = True
 
-genBoard(DISPLAYSURF)
+myBoard = Board()
+
+genBoard(DISPLAYSURF,myBoard.getBoard())
 
 while run: # main game loop
     for event in pygame.event.get():

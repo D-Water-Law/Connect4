ebdee13df6878df4ed725a1e1c7750ebd0dbab2b
Osas S <Osaghae2305@gmail.com>
2022-01-27 16:23:04 +0000

    Put RGB values in a variable and drew circle at the top of the board that follows mouse

--- connect4Pygame.py
+++ connect4Pygame.py
@@ -11,11 +11,29 @@ def genBoard(surface,board):
     squareSize = 100
     for row in range(rowCount):
         for col in range(colCount):
-            pygame.draw.rect(surface,(0,0,255),(col*squareSize,row*squareSize+squareSize,squareSize,squareSize))#############
-            pygame.draw.circle(surface,(0,0,0),(col*squareSize+(squareSize/2),row*squareSize+squareSize+(squareSize/2)),40)
+            pygame.draw.rect(surface,BLUE,(col*squareSize,row*squareSize+squareSize,squareSize,squareSize))
+            pygame.draw.circle(surface,BLACK,(col*squareSize+(squareSize/2),row*squareSize+squareSize+(squareSize/2)),40)
+
+def drawTopC(surface,x):
+    pygame.draw.circle(surface,RED,(x,50),40)
+
+def switchP(player):
+    if player == 1:
+        player += 1
+    else:
+        player -= 1
+    
+    return player
         
+########### Colours ##############
 
+BLUE = (0,0,255)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
+##################
 pygame.init()
 win_height = 700
 win_width = 600 
@@ -25,14 +43,18 @@ run = True
 
 myBoard = Board()
 
-genBoard(DISPLAYSURF,myBoard.getBoard())
 
 while run: # main game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
     
+    genBoard(DISPLAYSURF,myBoard.getBoard())
+    
+    mousex, mousey = pygame.mouse.get_pos()
+
+    drawTopC(DISPLAYSURF,mousex)
+
     activeKey = pygame.key.get_pressed()
     
     if activeKey[pygame.K_ESCAPE]:
@@ -40,6 +62,6 @@ while run: # main game loop
     
     
     pygame.display.update()  
-    
+    DISPLAYSURF.fill(BLACK)
 
 pygame.quit()
